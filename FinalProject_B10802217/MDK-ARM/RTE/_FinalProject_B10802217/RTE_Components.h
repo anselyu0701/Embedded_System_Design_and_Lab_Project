f8e99f1cbf6a7202fcf0e282fd55639be6360ce4
Ansel Lyu <38368384+anselyu0701@users.noreply.github.com>
2022-04-08 01:54:32 +0800

    Add files via upload

---
+++ FinalProject_B10802217/MDK-ARM/RTE/_FinalProject_B10802217/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'FinalProject_B10802217' 
+ * Target:  'FinalProject_B10802217' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
